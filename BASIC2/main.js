cbf2322150ae6e0028799c90db5b2d4c2dbf86d5
fugo8015276 <sf.engineer20231201@gmail.com>
2024-04-18 23:58:13 +0900

    新しいファイルを追加

--- BASIC2/main.js
+++ BASIC2/main.js
@@ -1,6 +1,26 @@
-console.log(count);
 let count = 0;
-console.log(count);
 count = 30;
-console.log(count);
 let newCount = 0;
+const daysInWeek = 7;
+let tomatoCount;
+
+let additionalResult = 2 + 5;
+let result = 5;
+result = result + 1;
+result += 1;
+//result++;
+//++result;
+
+let number = 10;
+number = -4;
+number = 32.42;
+let string = 'Hello';
+number = 'something';
+const userName = 'Yoshipi';
+string = 'Hello' + userName;
+string = `Hello ${userName}!`;
+string = ' \\ ';
+string = 'Hello\nYoshipi!';
+string = `Hello
+Yoshipi`;
+console.log(string);
---
+++ BASIC3/main.js
@@ -0,0 +1,38 @@
+const userInput = '10.9';
+let calcResult;
+calcResult = Number(userInput) + 10;
+console.log(calcResult);
+calcResult = parseInt(userInput) + 10;
+console.log(calcResult);
+calcResult = +userInput + 10;
+const tenNumber = 10;
+calcResult = '10' + String(tenNumber);
+calcResult = '10' + tenNumber.toString();
+console.log(calcResult);
+
+let boolean = true;
+boolean = false;
+
+let array = ['apple', 'banana', 'grape'];
+array = [1, 2, 3];
+array = [1, 'apple', true, array];
+array = ['apple', 'banana', 'grape'];
+console.log(array[2]);
+array = [];
+array.push('apple');
+console.log(array);
+
+const coffee = {
+  name: 'Chocolate Mocha',
+  size: 350,
+  isHot: true,
+  toppings: ['Cinnamon', 'Caramel'],
+  nutritions: {
+    calories: 430,
+    sugars: 53,
+  },
+};
+
+coffee.isHot = false;
+coffee.barista = 'Yoshipi';
+console.log(coffee);
